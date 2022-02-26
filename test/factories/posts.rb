ab7773b874679310a21d0339995d190047c0abcd
luis <hilario.huaman.6@gmail.com>
2022-02-26 17:37:57 -0500

    creacion de las prubas que verifiquen que la busqueda sea correcto

--- test/factories/posts.rb
+++ test/factories/posts.rb
@@ -5,4 +5,11 @@ FactoryBot.define do
     published { [true, false].sample }
     user
   end
+
+  factory :published_post, class: 'Post' do
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
+    published { true }
+    user
+  end
 end
